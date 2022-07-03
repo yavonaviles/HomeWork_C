52811d480ff7530e5f36eed4c442a2fc95de92ba
Yana <yavonaviles@gmail.com>
2022-07-03 11:24:58 +0300

    homework3

--- Program.cs
+++ Program.cs
@@ -167,7 +167,7 @@ void thirdNumber(){
 }
 thirdNumber();
 
-*/
+
 
 // 3 ЗАДАЧА. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
@@ -202,3 +202,60 @@ thirdNumber();
     }
 
     
+    
+    // ДЗ 3. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+internal class Program
+{
+    private const int V = 100000;
+
+    private static void Main(string[] args)
+    {
+        void palindrom()
+        {
+           
+            Console.Write("Введите пятизначное число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int num1 = number / 10000 % 10;
+            int num2 = number / 1000 % 10;
+            int rev1 = number / 10 % 10;
+            int rev2 = number % 10;
+            int nul = number / 10000;
+
+            if (nul < 1 || nul > 9)
+            {
+                Console.WriteLine(number + " -> " + "Не пятизначное число");
+            }
+            else if (num1 == rev2 && num2 == rev1)
+            {
+                Console.WriteLine(number + " -> " + "Палиндром");
+            }
+            else
+            {
+                Console.WriteLine(number + " -> " + "Не палиндром");
+            }
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            palindrom();
+        }
+    }
+}
+
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Input number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+void cube(){
+    int num = 1;
+    while(num <= N){
+        int result = num * num * num;
+        Console.Write( result + " " );
+        num++;
+    }
+}
+cube(); 
+
+*/
+
